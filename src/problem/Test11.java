36b72284ef7873788c6152a5d0c372a66ac43b21
SecretFr <linkclean@naver.com>
2021-06-24 14:44:50 +0900

    Info Practice

---
+++ src/problem/Test11.java
@@ -0,0 +1,32 @@
+package problem;
+import java.util.Scanner;
+class Info {
+	String name;
+	String addr;
+	String birth;
+	
+	Info(){};
+	Info(String name, String addr, String birth){
+		this.name = name;
+		this.addr = addr;
+		this.birth = birth;
+	}
+}
+
+public class Test11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Info p = new Info();
+		
+		p.name = sc.next();
+		p.addr = sc.next();
+		p.birth = sc.next();
+		
+		System.out.println("name : "+p.name);
+		System.out.println("address : "+p.addr);
+		System.out.println("birth : "+p.birth);
+	}
+
+}
