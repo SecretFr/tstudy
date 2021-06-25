4a0d6738ec1f23edbc71a7d424e5fcce7246bea6
SecretFr <linkclean@naver.com>
2021-06-25 15:17:31 +0900

    String Example

--- src/ByValue_Ref/Ex09_09.java
+++ src/ByValue_Ref/Ex09_09.java
@@ -17,6 +17,7 @@ public class Ex09_09 {
 		System.out.println("원 문자열4 ===> ["+str4.hashCode()+"]");
 		
 		System.out.println("문자열1==문자열2 결과 :\t"+(str1 == str2));
+		//항상 문자열은 equals로 비교하는 것이 좋다.
 		System.out.println("문자열1.equals(문자열2) 결과 :\t"+str1.equals(str2));
 		System.out.println("문자열1==문자열3 결과 :\t"+(str1 == str3));
 		System.out.println("문자열1.equals(문자열3) 결과 :\t"+str1.equals(str3));
