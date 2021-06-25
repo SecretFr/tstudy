7782468213f0f078e833ee1df7cbef31286b758e
SecretFr <linkclean@naver.com>
2021-06-25 14:42:41 +0900

    String Example

---
+++ src/ByValue_Ref/StringTest.java
@@ -0,0 +1,37 @@
+package ByValue_Ref;
+
+class Change{
+	String str;
+	public void changeString(Change c) {
+		c.str = "JSP";
+	}
+	
+}
+
+public class StringTest {
+	public String changeString(String src) {
+		src = "JSP";
+		return src;
+//		System.out.println(src);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("ABCD".toLowerCase());
+		System.out.println("ABCD".hashCode());
+		
+		Change c = new Change();
+		c.str = "JAVA";
+		
+		System.out.println(c.str);
+		c.changeString(c);
+		System.out.println(c.str);
+	
+		String step = "JAVA";
+		StringTest st = new StringTest();
+		
+		System.out.println(step);
+		step = st.changeString(step); // no change
+		System.out.println(step);
+		
+	}
+}
